79f07d366932ed9748f1327b9ac53af307e738d9
Charlie Moreno <cmorenorangel5453@conestogac.on.ca>
2022-09-28 18:37:06 -0400

    modified to keep the same convention that the IDE uses. fix the modules declarations

--- Core/Inc/ports.h
+++ Core/Inc/ports.h
@@ -11,22 +11,18 @@
 #ifndef INC_PORTS_H_
 #define INC_PORTS_H_
 
-
-
-#endif /* INC_PORTS_H_ */
-
 #include "common.h"
 #include "stm32l4xx_hal.h"
 
 /************************** Ports Definition **************************/
 
 /* RGB Led */
-#define LED_R_PORT		(GPIOA)
-#define LED_R_PIN		(GPIO_PIN_9)
-#define LED_G_PORT		(GPIOA)
-#define LED_G_PIN		(GPIO_PIN_10)
-#define LED_B_PORT		(GPIOA)
-#define LED_B_PIN		(GPIO_PIN_8)
+#define LED_R_GPIO_Port	(GPIOA)
+#define LED_R_Pin		(GPIO_PIN_9)
+#define LED_G_GPIO_Port	(GPIOA)
+#define LED_G_Pin		(GPIO_PIN_10)
+#define LED_B_GPIO_Port	(GPIOA)
+#define LED_B_Pin		(GPIO_PIN_8)
 
 
 /************************** Ports Routines **************************/
@@ -34,3 +30,7 @@
 void setPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 void resetPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 GPIO_PinState getPinState (GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+
+
+#endif /* INC_PORTS_H_ */
+
